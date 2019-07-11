033bb707e775dc13d44ac0a2f16db9db1203e5ec
Caio Eduardo <sr.cacaio.eduardo@gmail.com>
2019-07-10 21:13:14 -0300

    add for e var

---
+++ respostas-execicio2/resposta2-10.js
@@ -0,0 +1,15 @@
+let turno = prompt('Informe o turno');
+turno = turno.trim.charAt(0).toLowerCase();
+switch(turno) {
+    case 'm':
+        alert('Bom Dia!');
+        break;
+    case 'v':('Boa Tarde');
+        alert
+        break;
+    case 'n':
+        alert('Boa Noite');
+        break;
+    default:
+        alert('Operação inválida');
+}
