42e167a0419185964600b31a860fe6f7deb33f1b
Caio Eduardo <sr.cacaio.eduardo@gmail.com>
2019-07-15 21:11:23 -0300

    add repeticao 2

--- respostas-exercicio3/resposta3-3.js
+++ respostas-exercicio3/resposta3-3.js
@@ -1,13 +1,48 @@
+let nome = '';
 for (let i=0; i < 100; i++) {
-    let nome =prompt('Digite o seu nome');
-    let idade = prompt('Digite a sua idade');
-    if(idade >= 0 && idade <= 150);
-    let salario =prompt('Informe o seu salario mensal')
-    if (let) {
-        
+    nome =prompt('Digite o seu nome').trim();
+    let tamanhoNome=nome.lemgth
+    if(nome.lemgth>3){
+        break;
+    }else{
+        alert('Nome Inválido!!!');
     }
-    let sexo =('Informe o seu sexo')
-    if (let) {}
-    let status =('Informe o seu Estado Civil')
-    if (let) {}
 }
+    alert('nome: ' + nome);
+let idade = 0;
+for (let i = 0;i < 100; i++){
+    idade = prompt('Digite a sua idade').trim();
+    idade = parseInt(idade);
+    if (idade >= 0 && idade <=150){
+        break;
+    }else{
+            alert('Idade inválida!!!');
+        }
+    }
+alert('Idade ' + idade);
+
+let salario = 0;
+for (let i = 0; i < 100; i++){
+    salario =prompt('Informe o seu salario mensal').trim();
+    salario = parseInt(salario)
+    if(salario <= 0 && salario > 0){
+        break;
+    }else{
+        alert('Salario Inválido');
+    }
+}
+alert('Salário Mensal ' + salario)
+    
+
+let sexo = '';
+for (let i = 0; i < 3; i++)
+    sexo = prompt('Informe o seu sexo').trim();
+    sexo = parseInt(sexo)
+    if (sexo) {}
+
+
+    let status ='';
+    status=prompt('Informe o seu Estado Civil').trim();
+    if (status) {}
+
+
