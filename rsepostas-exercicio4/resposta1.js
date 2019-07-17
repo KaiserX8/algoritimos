126bbbefc55dd534310a327aea2492ba6cac5914
Caio Eduardo <sr.cacaio.eduardo@gmail.com>
2019-07-16 21:26:25 -0300

    add vetor

---
+++ rsepostas-exercicio4/resposta1.js
@@ -0,0 +1,11 @@
+let n1 = prompt('digite o 1º numero inteiro')
+
+let n2 = prompt('digite o 2º numero inteiro')
+
+let n3 = prompt('digite o 3º numero inteiro')
+
+let n4 = prompt('digite o 4º numero inteiro')
+
+let n5 = prompt('digite o 5º numero inteiro')
+
+alert(n1 + -n2 + -n3 + -n4 + -n5)
